1703f482994544fd1f6a704bc26950d29a7ae3fb
Ilona Demidenko <demiluri@gmail.com>
2018-09-22 10:33:29 +0200

    Filter todo items

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,6 +5,35 @@ import {Todo} from './Todo';
 
 class DashboardComponent extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            activeFilter: () => true
+        };
+
+        this.viewAll = this.viewAll.bind(this);
+        this.viewActive = this.viewActive.bind(this);
+        this.viewCompleted = this.viewCompleted.bind(this);
+    }
+
+    viewAll() {
+        this.setState({
+            activeFilter: () => true
+        })
+    }
+
+    viewActive() {
+        this.setState({
+            activeFilter: (todo) => !todo.completed
+        })
+    }
+
+    viewCompleted() {
+        this.setState({
+            activeFilter: (todo) => todo.completed
+        })
+    }
+
     componentDidMount() {
         this.props.loadTodos();
     }
@@ -12,17 +41,23 @@ class DashboardComponent extends React.Component {
     render() {
         const {todos, completeTodo, updateTodo, deleteTodo} = this.props;
         return (
-            <ul>
-                {todos.map(todo =>
-                    <li key={todo.id}>
-                        <Todo todo={todo}
-                              completeTodo={completeTodo}
-                              updateTodo={updateTodo}
-                              deleteTodo={deleteTodo}
-                        />
-                    </li>
-                )}
-            </ul>
+            <div>
+                <a href="#" onClick={() => this.viewAll()}>View All</a>
+                <a href="#" onClick={() => this.viewActive()}>Active</a>
+                <a href="#" onClick={() => this.viewCompleted()}>Completed</a>
+
+                <ul>
+                    {todos.filter(this.state.activeFilter).map(todo =>
+                        <li key={todo.id}>
+                            <Todo todo={todo}
+                                  completeTodo={completeTodo}
+                                  updateTodo={updateTodo}
+                                  deleteTodo={deleteTodo}
+                            />
+                        </li>
+                    )}
+                </ul>
+            </div>
         );
     }
 };
